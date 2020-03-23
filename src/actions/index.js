5e0e14ccebfe60090a5b3915fe297eb8a0014532
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-23 18:12:17 +0100

    add postList component, userReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,20 +1,26 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
-
-export const fetchPosts =  () => {
-
+export const fetchPosts =  () => async dispatch => { 
+    const response = await jsonPlaceholder.get('/posts'); 
+    
+    // up short, down longer
+    
     // return async function(dispatch, getState){
     //     const response = await jsonPlaceholder.get('/posts');
     // }
-    // up longer, down short
-    return async dispatch => { const response = await jsonPlaceholder.get('/posts'); 
-    
+      
     dispatch({type: "FETCH_POSTS", payload: response.data })
-
-    };
         // up short, down longer
         // return {
         //     type: "FETCH_POSTS",
         //     payload: response
         // };
 };
+
+export const fetchUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get(`users/${id}`)
+        
+    dispatch({type: "FETCH_USER", payload: response.data})
+    
+
+}
