dfd3b2b47344aedc4c91fba64897a57269c995bf
VukasinVujic <vukasinvujic021@gmail.com>
2020-03-24 08:43:31 +0100

    _.memoize,lodach library,how to force only one HTTP request, prevent getting one same user more times as HTTP respond

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash'
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts =  () => async dispatch => { 
@@ -17,10 +18,13 @@ export const fetchPosts =  () => async dispatch => {
         // };
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch); // one function lala***
+// normal version down , short up
+// {    _fetchUser(id, dispatch);}
+
+const _fetchUser = _.memoize(async (id, dispatch) => { // lala*** is callling this function
     const response = await jsonPlaceholder.get(`users/${id}`)
         
     dispatch({type: "FETCH_USER", payload: response.data})
-    
 
-}
+});
